f88a81ccf51d114d1cc15fc9e82a935ba741a6de
Alexy845 <alexy.daubresse@ynov.com>
2022-09-07 14:26:21 +0200

    b

---
+++ printdigits/isnegative.go
@@ -0,0 +1,16 @@
+package main
+
+import "piscine"
+
+func IsNegative(nb int) {
+	if nb >= 0;
+		return z01.PrintRune(rune(84))
+	else;
+		return z01.PrintRune(rune(70))
+}
+
+func main() {
+	piscine.IsNegative(1)
+	piscine.IsNegative(0)
+	piscine.IsNegative(-1)
+}
