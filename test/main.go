ecac97ab5e3c09541a716840d0e17628ee91422b
Alexy845 <alexy.daubresse@ynov.com>
2022-09-07 14:46:17 +0200

    b

--- test/main.go
+++ test/main.go
@@ -2,13 +2,6 @@ package main
 
 import "piscine"
 
-func IsNegative(nb int) {
-	if nb >= 0;
-		return z01.PrintRune(rune(84))
-	else;
-		return z01.PrintRune(rune(70))
-}
-
 func main() {
 	piscine.IsNegative(1)
 	piscine.IsNegative(0)
