20b16ef65ccfe90527d22b7d31a58b8e69457f21
Alexy845 <alexy.daubresse@ynov.com>
2022-09-07 15:01:01 +0200

    b

--- isnegative.go
+++ isnegative.go
@@ -5,10 +5,9 @@ import "github.com/01-edu/z01"
 func IsNegative(nb int) {
 	if nb >= 0 {
 		z01.PrintRune(rune(84))
-
+		z01.PrintRune('\n')
 	} else {
 		z01.PrintRune(rune(70))
-
+		z01.PrintRune('\n')
 	}
-	z01.PrintRune('\n')
 }
