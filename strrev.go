6a945d88ceff8968f778e16b7fe4e390f431f87e
Alexy845 <alexy.daubresse@ynov.com>
2022-09-08 15:44:38 +0200

    b

--- strrev.go
+++ strrev.go
@@ -1,10 +1,10 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func StrRev(s string) string {
 	sString := []rune(s)
+	f := ""
 	for c := len(sString) - 1; c >= 0; c-- {
-		z01.PrintRune(sString[c])
+		f += string(sString[c])
 	}
+	return f
 }
