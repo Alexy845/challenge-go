d49f7681d2c843792f779d99eae50285d8e7c9ea
Alexy845 <alexy.daubresse@ynov.com>
2022-09-15 12:28:44 +0200

    finis

--- index.go
+++ index.go
@@ -1,12 +1,14 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	for t := range toFind {
+	if len(toFind) == 0 {
+		return -1
+	}
+
+	for t := range s {
 		var l string = s[t : len(toFind)-1]
 		if l == toFind {
 			return t
-		} else if len(toFind) == 0 {
-			return -1
 		}
 	}
 	return -1
