ec111610caf5f9f1bd4e3854f9a7beb0769d1311
Alexy845 <alexy.daubresse@ynov.com>
2022-09-15 10:18:40 +0200

    finis

--- alphacount.go
+++ alphacount.go
@@ -3,10 +3,11 @@ package piscine
 func AlphaCount(s string) int {
 	c := []rune(s)
 	e := 0
-	for i := range c {
-		if i >= 97 || i <= 122 {
+	for _, i := range c {
+		i += 1
+		if i >= 97 && i <= 122 || i >= 65 && i <= 90 {
 			e += 1
 		}
 	}
-	return 0
+	return e
 }
