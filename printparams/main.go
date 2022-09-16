afc9ed44a4545edc8fddf56890a9911318314e20
Alexy845 <alexy.daubresse@ynov.com>
2022-09-16 16:41:34 +0200

    finis

---
+++ printparams/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := []rune(os.Args[1])
+	for _, t := range arguments[2:] {
+		z01.PrintRune(t)
+	}
+	z01.PrintRune('\n')
+}
