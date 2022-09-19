3ca78df04d729f792d56fd6ca746873e7cfd2eb2
Alexy845 <alexy.daubresse@ynov.com>
2022-09-19 16:42:24 +0200

    finis

--- makerange.go
+++ makerange.go
@@ -1,10 +1,11 @@
 package piscine
 
 func MakeRange(min, max int) []int {
+	o := make([]int, max-min)
 	if min > max {
 		return []int(nil)
 	}
-	o := make([]int, max-min)
+
 	for i := 0; i < max-min; i++ {
 		o[i] = i + min
 	}
