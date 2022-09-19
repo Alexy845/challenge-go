ab3433d01b54e06db037516452a78714a1d1f90a
Alexy845 <alexy.daubresse@ynov.com>
2022-09-19 11:25:59 +0200

    finis

---
+++ revparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	lst := os.Args[:1]
+	for _, c := range lst {
+		arr := []rune(c)                // Conversion de Args[0] (qui contient le programmname) en tableau de rune
+		for i := 0; i < len(arr); i++ { // On parcourt le tableau à partir de la seconde position pour esquiver le ./
+			z01.PrintRune(arr[i])
+		}
+		z01.PrintRune('\n')
+	}
+}
