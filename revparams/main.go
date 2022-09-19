3f076033b8566ce6b029386fcd6a9c609d3a5edc
Alexy845 <alexy.daubresse@ynov.com>
2022-09-19 14:34:50 +0200

    finis

--- revparams/main.go
+++ revparams/main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
-	lst := os.Args[1:]
-	for _, c := range lst {
-		arr := []rune(c)                // Conversion de Args[0] (qui contient le programmname) en tableau de rune
-		for i := len(arr); i > 0; i-- { // On parcourt le tableau à partir de la seconde position pour esquiver le ./
-			z01.PrintRune(arr[i])
+	argt := os.Args
+	for i := len(argt) - 1; i > 0; i-- {
+		if i != 0 {
+			for _, k := range argt[i] {
+				z01.PrintRune(k)
+			}
+			z01.PrintRune('\n')
 		}
-		z01.PrintRune('\n')
 	}
 }
