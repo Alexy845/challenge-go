6d67d80514cef422f909f652dce62bbe1f5de47c
Alexy845 <alexy.daubresse@ynov.com>
2022-09-20 13:12:08 +0200

    finis

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,20 +1,19 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	a := 0
-	b := 0
-	c := []string{}
-	for i, d := range s {
-		if d == ' ' {
-			if s[i-1] == ' ' {
-				b = i + 2
-			} else {
-				b = i
-				c = append(c, s[a:b])
-				a = i + 1
+	srune := []rune(s)
+	lst := []string{}
+	str := ""
+	for i := 0; i < len(srune); i++ {
+		if srune[i] == ' ' || srune[i] == '\t' || srune[i] == '\n' {
+			if str != "" {
+				lst = append(lst, str)
 			}
+			str = ""
+		} else {
+			str += string(srune[i])
 		}
 	}
-	c = append(c, s[a:])
-	return c
+	lst = append(lst, str)
+	return lst
 }
