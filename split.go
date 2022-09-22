129645f61632cd62f24ccdf5e35f8c64e360bc3a
Alexy845 <alexy.daubresse@ynov.com>
2022-09-22 09:52:08 +0200

    finis

--- split.go
+++ split.go
@@ -4,7 +4,7 @@ func Split(s, sep string) []string {
 	counter := 0
 	str := ""
 	tab := []string{}
-	for i := 0; i < len(s)-len(sep); i++ {
+	for i := 0; i <= len(s)-len(sep); i++ {
 		if counter > 0 {
 			counter--
 			continue
