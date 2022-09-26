038a58a4ab9f3a44332da4e4022af902a3332df4
Alexy845 <alexy.daubresse@ynov.com>
2022-09-26 14:20:44 +0200

    finis

--- boolean/main.go
+++ boolean/main.go
@@ -23,7 +23,6 @@ func isEven(nbr int) bool {
 
 func main() {
 	if isEven(len(os.Args)) == true {
-
 		printStr("I have an even number of arguments")
 	} else {
 		printStr("I have an odd number of arguments")
