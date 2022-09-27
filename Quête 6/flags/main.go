f1262a8f90c6eb7ea0909f4c438e5f5020f80645
Alexy845 <alexy.daubresse@ynov.com>
2022-09-27 14:59:53 +0200

    finis

--- Quête 6/flags/main.go
+++ Quête 6/flags/main.go
@@ -1,85 +1,72 @@
 package main
 
 import (
-    "os"
-
-    "fmt"
+	"fmt"
+	"os"
 )
 
 func main() {
-    if len(os.Args) == 1 || os.Args[1] == "--help" || os.Args[1] == "-h" {
-        fmt.Println("--insert
-        -i
-               This flag inserts the string into the string passed as argument.
-      --order
-        -o
-               This flag will behave like a boolean, if it is called it will order the argument.")
-        return
-    }
-    args := os.Args[1:]
-    insert := false
-    toInsert := ""
-    var str string
-    var nb []int
-    order := false
-    if len(args) > 1 {
-        if (len(args[0]) > 10 && args[0][0:9] == "--insert") || (len(args[0]) > 4 && args[0][0:3] == "-i=") {
-            insert = true
-            toInsert = args[0][Index(args[0], "=")+1:]
-        }
-        if (args[0] == "--order" || args[0] == "-o") || (args[1] == "--order" || args[1] == "-o") {
-            order = true
-        }
-    } else {
-        str = args[0]
-    }
-    if insert {
-        if order {
-            str = args[2] + toInsert
-        } else {
-            str = args[1] + toInsert
-        }
-    }
-    if order {
-        if !insert {
-            str = args[1]
-        }
-        for _, c := range str {
-            nb = append(nb, int(rune(c)))
-        }
-        SortIntegerTable(nb)
-        str = ""
-        for _, v := range nb {
-            str += string(rune(v))
-        }
-    }
-    fmt.Println(str)
+	command := os.Args
+	GetFlags(command)
 }
 
-func SortIntegerTable(table []int) {
-    for i := 0; i < len(table); i++ {
-        for j := i + 1; j < len(table); j++ {
-            if table[i] > table[j] {
-                tmp := table[i]
-                table[i] = table[j]
-                table[j] = tmp
-            }
-        }
-    }
+func Insert(strOrigine string, strToAdd string) string {
+	return strOrigine + strToAdd // Problème avec le test go run . "-i=Q 0d 6JI S 4j" "lsJ  FhbrkYGN"
 }
 
-func Index(s string, toFind string) int {
-    if len(toFind) == 0 || len(s) == 0 {
-        return 0
-    }
-    for v := range s {
-        if v+len(toFind) >= len(s) {
-            break
-        }
-        var slice string = s[v : v+len(toFind)]
-        if slice == toFind {
-            return v
-        }
-    }
-    return -1
+func Order(strToSort string) string {
+	runeSort := []rune(strToSort)
+	for i := 0; i < len(runeSort); i++ {
+		for j := i + 1; j < len(runeSort); j++ {
+			if runeSort[i] > runeSort[j] {
+				runeSort[i], runeSort[j] = runeSort[j], runeSort[i]
+			}
+		}
+	}
+	c := ""
+	for _, i := range runeSort {
+		c += string(i)
+	}
+	return c
+}
+func Help() {
+	fmt.Println("--insert")
+	fmt.Println("  -i")
+	fmt.Println("     This flag inserts the string into the string passed as argument.")
+	fmt.Println("--order")
+	fmt.Println("  -o")
+	fmt.Println("     This flag will behave like a boolean, if it is called it will order the argument.")
+}
+func GetFlags(command []string) {
+	if len(command) == 1 {
+		Help()
+	} else {
+		if command[1] == "--help" || command[1] == "-h" {
+			Help()
+		} else if len(command[1]) > 8 {
+			if command[1][:8] == "--insert" {
+				if command[2] == "--order" || command[2] == "-o" {
+					fmt.Println(Order(Insert(command[3], command[1][9:])))
+				} else {
+					fmt.Println(Insert(command[2], command[1][9:]))
+				}
+			} else if command[1][:2] == "-i" {
+				if command[2] == "--order" || command[2] == "-o" {
+					fmt.Println(Order(Insert(command[3], command[1][3:])))
+				} else {
+					fmt.Println(Insert(command[2], command[1][3:]))
+				}
+			}
+		} else if command[1][:2] == "-i" {
+			if command[2] == "--order" || command[2] == "-o" {
+				fmt.Println(Order(Insert(command[3], command[1][3:])))
+			} else {
+				fmt.Println(Insert(command[2], command[1][3:]))
+			}
+		} else if command[1] == "--order" || command[1] == "-o" {
+			fmt.Println(Order(command[2]))
+		} else {
+			fmt.Println(command[1])
+		}
+	}
 }
